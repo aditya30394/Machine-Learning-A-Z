4c70e65af5d82bda18f0e5b220f4d80781099f09
Aditya Kumar <aditya30394@gmail.com>
2018-07-13 09:22:36 +0530

    Adding the regression template

--- Part 2 - Regression/Section 6 - Polynomial Regression/practice_polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/practice_polynomial_regression.py
@@ -41,3 +41,19 @@ plt.title("Truth or Bluff (Linear Regression)")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
 plt.show()
+
+
+# To get a continuous graph
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, poly_reg_object.predict( poly_reg.fit_transform(X_grid)), color='blue')
+plt.title("Truth or Bluff (Linear Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+#Final result comparision
+
+linear_reg.predict(6.5)
+poly_reg_object.predict(poly_reg.fit_transform(6.5))

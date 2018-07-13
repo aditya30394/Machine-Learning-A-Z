014d659954c05dbfb3e21c1d74659bb7e1b7c11a
Aditya Kumar <aditya30394@gmail.com>
2018-07-13 09:05:35 +0530

    Adding the code with the implementation of polynomial regression (with 4 degree of freedom)

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/practice_polynomial_regression.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 08:20:19 2018
+
+@author: Aditya Kumar
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# no need to split the data into test and train as data is very less
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+linear_reg = LinearRegression()
+linear_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg_object = LinearRegression()
+poly_reg_object.fit(X_poly, y)
+
+# Visualising the Linear Regression results
+plt.scatter(X,y,color='red')
+plt.plot(X, linear_reg.predict(X), color='blue')
+plt.title("Truth or Bluff (Linear Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+# Visualising the Polynomial Regression results
+plt.scatter(X, y, color='red')
+plt.plot(X, poly_reg_object.predict( poly_reg.fit_transform(X)), color='blue')
+plt.title("Truth or Bluff (Linear Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
